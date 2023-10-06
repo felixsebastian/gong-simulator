d99eaf66b40b0974312bfcec22178ce649639144
Felix Sebastian <hi@felixsebastian.dev>
2023-10-06 13:04:00 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -1,67 +1,93 @@
+"use strict";
+
 const size = 500;
-const a = document.getElementById("app");
+const app = document.getElementById("app");
 const ringing = document.getElementById("ringing");
-let start = null;
+let lastHitStart = null;
+let audioElements = document.getElementsByTagName("audio")[0].children;
+audioElements = Array.from(audioElements);
+const soundUrls = audioElements.map((e) => e.src);
 
-const sounds = Array.from(
-  document.getElementsByTagName("audio")[0].children
-).map((a) => a.src);
-
-console.log("sounds", sounds);
-
-a.onmousedown = (e) => {
-  start = { x: e.clientX, y: e.clientY, time: new Date().getTime() };
+// record mousedown information so that we can compare with mouseup
+app.onmousedown = (event) => {
+  lastHitStart = {
+    x: event.clientX,
+    y: event.clientY,
+    time: new Date().getTime()
+  };
 };
 
-const linearAccuracy = (v) => 1 - Math.abs(v / (size / 2) - 1);
-const limit = (max) => (v) => Math.min(v / max, 1);
+const limitedFraction = (v, max) => Math.min(v / max, 1);
 
-const cartesianDistance = (x1, x2, y1, y2) =>
-  Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+const cartesianDistance = (x1, x2, y1, y2) => {
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+}
 
-const getTime = () =>
-  Math.max(1 - limit(700)(new Date().getTime() - start.time) + 0.4, 1);
+// returns a fraction between 0.4 and 1
+const getTime = () => {
+  const max = 700
+  const min = 0.4
+  const fractionOfMax = limitedFraction(new Date().getTime() - start.time, max)
+  return Math.max(1 - fractionOfMax + min, 1);
+}
 
-const getAccuracy = (e) =>
-  linearAccuracy(e.clientX) * linearAccuracy(e.clientY);
+const getAxisDistanceFromCenter = (v) => 1 - Math.abs(v / (size / 2) - 1);
 
-const getDistance = (e) =>
-  limit(200)(
-    Math.round(cartesianDistance(start.x, e.clientX, start.y, e.clientY))
-  );
+const getAccuracy = (e) => {
+  const xDistance = getAxisDistanceFromCenter(e.clientX);
+  const yDistance = getAxisDistanceFromCenter(e.clientY)
+  return xDistance * yDistance;
+}
+
+const getDistance = (e) => {
+  const hitDistance = cartesianDistance(start.x, e.clientX, start.y, e.clientY)
+  return limitedFraction(Math.round(hitDistance), 200);
+}
 
 const getSwing = (e) => getTime() * getAccuracy(e) * getDistance(e) * 100;
 
-const getHitpoint = (e) =>
-  (1 - limit(250)(cartesianDistance(e.clientX, 250, e.clientY, 240))) * 100;
+const getHitpoint = (e) => {
+  const distance = cartesianDistance(e.clientX, 250, e.clientY, 240)
+  const fractionOfMax = limitedFraction(distance, 250)
+  return (1 - fractionOfMax) * 100;
+}
 
 const getLoudness = (e) => Math.round((getSwing(e) + getHitpoint(e)) / 2);
 
+const playAudio = (loudness) => {
+  const audioFile = soundUrls[Math.floor(loudness / (100 / soundUrls.length))];
+  new Audio(audioFile).play();
+  start = null;
+}
+
 window.onmouseup = (e) => {
-  if (!start) return;
+  if (!lastHitStart) return;
   const loudness = getLoudness(e);
   if (loudness === 0) return;
   ringing.className = "enabled";
   ringing.style.transition = `opacity ${loudness / 8}s ease-out`;
-
-  window.setTimeout(() => {
-    ringing.className = "";
-  }, 10);
-
-  const file = sounds[Math.floor(loudness / (100 / sounds.length))];
-  new Audio(file).play();
-  start = null;
+  window.setTimeout(() => { ringing.className = ""; }, 10);
+  playAudio(loudness)
 };
 
-let pollLoadedInterval = window.setInterval(() => {
-  if (
-    sounds.every((a) => new Audio(a).networkState === 3) &&
-    Array.from(document.getElementsByClassName("preloadimage")).every(
-      (i) => i.complete
-    )
-  ) {
-    document.getElementById("content").style.display = "block";
-    document.getElementById("loader").style.display = "none";
-    window.clearInterval(pollLoadedInterval);
-  }
+const soundsDoneLoading = () => {
+  return soundUrls.every((a) => new Audio(a).networkState === 3)
+}
+
+const imagesDoneLoading = () => {
+  let imageElements = document.getElementsByClassName("preloadimage")
+  imageElements = Array.from(imageElements)
+  return imageElements.every((image) => image.complete)
+}
+
+let pollInterval
+
+const initialize = () => {
+  document.getElementById("content").style.display = "block";
+  document.getElementById("loader").style.display = "none";
+  window.clearInterval(pollInterval);
+}
+
+pollInterval = window.setInterval(() => {
+  if (soundsDoneLoading() && imagesDoneLoading()) initialize()
 }, 300);
